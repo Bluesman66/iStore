1be4a080b0d1d94de411f01df06b91822e71f751
Alex Paclin <alex.paclin@gmail.com>
2015-05-02 16:39:14 +0500

    start

---
+++ iStore.WebUI/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Optimization;
+
+namespace iStore.WebUI
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/css/font-awesome.css",
+                "~/Content/styles.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/knockout-{version}.js",
+                "~/Scripts/knockout.mapping-latest.js",
+                "~/Scripts/perpetuum.knockout.js",
+                "~/Scripts/bootstrap.js"));
+        }
+    }
+}
---
+++ iStore.WebUI/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using PerpetuumSoft.Knockout;
+
+namespace iStore.WebUI
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            ModelBinders.Binders.DefaultBinder = new KnockoutModelBinder();
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
