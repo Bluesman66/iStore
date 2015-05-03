f14d3dfc61cea3fdcb25ed634234faaec1e2bd22
Alex Paclin <alex.paclin@gmail.com>
2015-05-03 16:05:43 +0500

    menu

--- iStore.WebUI/Scripts/main.js
+++ iStore.WebUI/Scripts/main.js
@@ -1,8 +1,28 @@
-﻿$(function() {
-    $('.store-menu-lg li, .store-menu-md li, .store-menu-sm li').hover(function () {
+﻿$(function () {
+    $('.store-menu-lg li, .store-menu-md li, .store-menu-sm li').hover(
+        function () {
+            $(this).css('cursor', 'pointer');
             $(this).find('a').css('color', 'grey');
         },
-        function() {
+        function () {
             $(this).find('a').css('color', '');
+            $(this).css('cursor', 'default');
+        }).click(function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            var href = $(this).find('a').attr('href');
+            if (href) {
+                window.location = href;
+            }
+        });
+
+    $('.store-menu-lg li > a, .store-menu-md li > a, .store-menu-sm li > a').click(
+        function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            var href = $(this).attr('href');
+            if (href) {
+                window.location = href;
+            }
         });
 })
